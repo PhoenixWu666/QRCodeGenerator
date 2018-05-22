6d99ce9cbef51693b437c4c4addf38a65adaeced
Phoenix Wu <phoenix@funpodium.net>
2018-05-22 20:02:23 +0800

    Initial Commit

---
+++ QRCodeGenerator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  QRCodeGenerator
+//
+//  Created by Phoenix Wu on H30/05/22.
+//  Copyright © 平成30年 Phoenix Wu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
